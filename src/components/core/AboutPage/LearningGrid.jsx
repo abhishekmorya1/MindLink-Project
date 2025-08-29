1a1bf30b025af4542f81e0759923ac37f92d4dd3
Abhishek Morya <abhimorya8826@gmail.com>
2025-08-29 19:44:28 +0530

    first commit

---
+++ src/components/core/AboutPage/LearningGrid.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import HighlightText from "../../../components/core/HomePage/HighlightText";
+import CTAButton from "../../../components/core/HomePage/Button";
+
+const LearningGridArray = [
+  {
+    order: -1,
+    heading: "World-Class Learning for",
+    highliteText: "Anyone, Anywhere",
+    description:
+      "MindLink partners with more than 275+ leading universities and companies to bring flexible, affordable, job-relevant online learning to individuals and organizations worldwide.",
+    BtnText: "Learn More",
+    BtnLink: "/",
+  },
+  {
+    order: 1,
+    heading: "Curriculum Based on Industry Needs",
+    description:
+      "Our courses are developed in partnership with leading companies to ensure you learn skills that are in high demand in today’s job market.",
+  },
+  {
+    order: 2,
+    heading: "Innovative Learning Methods",
+    description:
+      "Experience engaging content through videos, quizzes, and interactive projects designed to boost your understanding and retention.",
+  },
+  {
+    order: 3,
+    heading: "Certification",
+    description:
+      "Receive verified certificates upon course completion that can enhance your resume and help you stand out to employers.",
+  },
+  {
+    order: 4,
+    heading: `Auto-Grading & Instant Feedback`,
+    description:
+      "Our system instantly evaluates your assignments and quizzes, providing real-time feedback to accelerate your learning progress.",
+  },
+  {
+    order: 5,
+    heading: "Ready to Work",
+    description:
+      "Gain practical experience through hands-on projects and connect with industry mentors to prepare for your future career.",
+  },
+];
+
+const LearningGrid = () => {
+  return (
+    <div className="grid mx-auto w-[350px] xl:w-fit grid-cols-1 xl:grid-cols-4 mb-12">
+      {LearningGridArray.map((card, i) => {
+        return (
+          <div
+            key={i}
+            className={`${i === 0 && "xl:col-span-2 xl:h-[294px]"}  ${
+              card.order % 2 === 1
+                ? "bg-richblack-700 h-[294px]"
+                : card.order % 2 === 0
+                ? "bg-richblack-800 h-[294px]"
+                : "bg-transparent"
+            } ${card.order === 3 && "xl:col-start-2"}  `}
+          >
+            {card.order < 0 ? (
+              <div className="xl:w-[90%] flex flex-col gap-3 pb-10 xl:pb-0">
+                <div className="text-4xl font-semibold ">
+                  {card.heading}
+                  <HighlightText text={card.highliteText} />
+                </div>
+                <p className="text-richblack-300 font-medium">
+                  {card.description}
+                </p>
+
+                <div className="w-fit mt-2">
+                  <CTAButton active={true} linkto={card.BtnLink}>
+                    {card.BtnText}
+                  </CTAButton>
+                </div>
+              </div>
+            ) : (
+              <div className="p-8 flex flex-col gap-8">
+                <h1 className="text-richblack-5 text-lg">{card.heading}</h1>
+
+                <p className="text-richblack-300 font-medium">
+                  {card.description}
+                </p>
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default LearningGrid;
---
+++ src/components/core/Dashboard/AddCourse/Upload.jsx
@@ -0,0 +1,121 @@
+import { useEffect, useRef, useState } from "react"
+import { FiUploadCloud } from "react-icons/fi"
+import { useSelector } from "react-redux"
+
+import "video-react/dist/video-react.css"
+import { Player } from "video-react"
+
+export default function Upload({
+  name,
+  label,
+  register,
+  setValue,
+  errors,
+  video = false,
+  viewData = null,
+  editData = null,
+}) {
+  const { course } = useSelector((state) => state.course)
+  const [selectedFile, setSelectedFile] = useState(null)
+  const [previewSource, setPreviewSource] = useState(
+    viewData ? viewData : editData ? editData : ""
+  )
+  const inputRef = useRef(null)
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0]
+    if (file) {
+      previewFile(file)
+      setSelectedFile(file)
+    }
+  }
+
+  const previewFile = (file) => {
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onloadend = () => {
+      setPreviewSource(reader.result)
+    }
+  }
+
+  useEffect(() => {
+    register(name, { required: true })
+  }, [register])
+
+  useEffect(() => {
+    setValue(name, selectedFile)
+  }, [selectedFile, setValue])
+
+  return (
+    <div className="flex flex-col space-y-2">
+      <label className="text-sm text-richblack-5" htmlFor={name}>
+        {label} {!viewData && <sup className="text-pink-200">*</sup>}
+      </label>
+
+      <div className="flex min-h-[250px] items-center justify-center rounded-md border-2 border-dotted border-richblack-500 bg-richblack-700">
+        {previewSource ? (
+          <div className="flex w-full flex-col p-6">
+            {!video ? (
+              <img
+                src={previewSource}
+                alt="Preview"
+                className="h-full w-full rounded-md object-cover"
+              />
+            ) : (
+              <Player aspectRatio="16:9" playsInline src={previewSource} />
+            )}
+            {!viewData && (
+              <button
+                type="button"
+                onClick={() => {
+                  setPreviewSource("")
+                  setSelectedFile(null)
+                  setValue(name, null)
+                }}
+                className="mt-3 text-richblack-400 underline"
+              >
+                Cancel
+              </button>
+            )}
+          </div>
+        ) : (
+          <div className="flex w-full flex-col items-center p-6">
+            <input
+              type="file"
+              accept={!video ? "image/*" : "video/mp4"}
+              onChange={handleFileChange}
+              ref={inputRef}
+              className="hidden"
+            />
+            <div
+              className="grid aspect-square w-14 place-items-center rounded-full bg-pure-greys-800 cursor-pointer"
+              onClick={() => inputRef.current && inputRef.current.click()}
+            >
+              <FiUploadCloud className="text-2xl text-yellow-50" />
+            </div>
+            <p className="mt-2 max-w-[200px] text-center text-sm text-richblack-200">
+              Click to{" "}
+              <span
+                className="font-semibold text-yellow-50 cursor-pointer"
+                onClick={() => inputRef.current && inputRef.current.click()}
+              >
+                Browse
+              </span>{" "}
+              and select a {!video ? "image" : "video"}
+            </p>
+            <ul className="mt-10 flex list-disc justify-between space-x-12 text-center text-xs text-richblack-200">
+              <li>Aspect ratio 16:9</li>
+              <li>Recommended size 1024x576</li>
+            </ul>
+          </div>
+        )}
+      </div>
+
+      {errors[name] && (
+        <span className="ml-2 text-xs tracking-wide text-pink-200">
+          {label} is required
+        </span>
+      )}
+    </div>
+  )
+}
